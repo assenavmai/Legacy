37abaf2b22ed1abaebe8825d993e7ebbc819ca64
Vanessa White <whitevanessa14@gmail.com>
2015-02-21 21:41:52 -0500

    Trying to read from file

--- A2/hangman.adb
+++ A2/hangman.adb
@@ -7,30 +7,41 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with ada.strings.unbounded; use ada.strings.unbounded;
 with ada.strings.unbounded.Text_IO; use ada.strings.unbounded.Text_IO;
+with ada.Numerics.discrete_Random;
 
 procedure Hangman is
 
-str : constant string := "jedi";
-line : string(1..5);
-len, i: integer := 0;
-type tab is array(1..1000) of unbounded_string;
-p : tab;
-s : unbounded_string;
+	str : constant string := "jedi";
+	len, i: integer := 0;
+	filestr : unbounded_string;
+	fp : file_type;
+	type arr is array (1..1000) of unbounded_string;
+	wordarr : arr;
+	type rndRange is range 1..3;
+	package rnd is new Ada.Numerics.Discrete_Random(rndRange);
+	rndGenerator : rnd.Generator;
+	num : rndRange;
 
 begin
 
-put(str);new_line;
+	rnd.Reset(rndGenerator);
+	num := rnd.Random(rndGenerator);
+	put_line(rndRange'image(num));
 
-line := "hello";
-put(line);
+	open(fp, in_file, "test.txt");
 
---Getting the string length of the word
+		for i in 1..5 loop
+			exit when end_of_line(fp);
+			get_line(fp, filestr);
+			wordarr(i) := filestr;
+			put_line((wordarr(i)));
+			len := length(filestr);
+			put(len);
+			put(filestr);
+			put(wordarr(i));
+			new_line;
+		end loop;
+	close(fp);
 
-for i in 1..5 loop
-	get_line(s);
-	p(i) := s;
-	put_line(p(i));
-	put(length(s));
-end loop;
 
 end Hangman;
