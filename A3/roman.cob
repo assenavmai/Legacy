e3fb45e803b709d5b8e88f66bdb8e7580722e1b9
Vanessa White <whitevanessa14@gmail.com>
2015-03-12 16:53:10 -0400

    Tweaking little things

--- A3/roman.cob
+++ A3/roman.cob
@@ -1,17 +1,17 @@
 * Program to convert roman numerals to its equivalent decimal value.
-* Migrated from older Cobol code.
+* Migrated from legacy Cobol code.
 * Author: Vanessa White
 * Course: CIS*3190
 * Date last edited: March 8th, 2015.
 
 identification division.
-program-id. cob.
+program-id. getvalue.
 environment division.
 input-output section.
 file-control.
 	select standard-input assign to keyboard.
 	select standard-output assign to display.
-	select ifile assign to "numeral.in"
+	select ifile assign to filename
 		organization is line sequential.
 data division.
 file section.
@@ -21,20 +21,26 @@ fd standard-output.
 	01 stdout-record   pic x(80).
 fd ifile.
 	01 input-record.
-		05 numeral 	pic x(1) occurs 30 times.
+		05 numeral 	pic x(30).
+		05 filename pic x(30).
 working-storage section.
 01 array-area.
-	02 letter pic x(1) occurs 30 times.
+	02 letter pic x occurs 30 times.
 77	i pic S99 usage is computational.
 77	val pic S9(4) usage is computational.
 77	summation pic S9(8) usage is computational.
 77 	prev pic S9(8) usage is computational.
 77 	str pic x(30).
 77	eof-switch pic 9 value 1.
+77 	counter pic 999 value 0.
+77	invalidFlag pic 9 value 0.
+
+
 procedure division.
 
 	open input standard-input, output standard-output.
 
+*	Show an introduction to the program for the user
 	display ""
 	display "Welcome to the Roman Numeral Convertor."
 	display "Enter a roman numeral statement and it will be converted to its equivalent decimal value"
@@ -42,31 +48,34 @@ procedure division.
 	display "Enter 'q' at anytime to quit."
 	display ""
 
-perform until letter(i) = 'q'
+* Continue to prompt the user to enter input until they type 'q'
+	perform until letter(i) = 'q'
 
-	move 0 to prev
-	move 1 to i
-	move 0 to summation
+		move 0 to prev
+		move 1 to i
+		move 0 to summation
 
-	display "Enter roman numerals, read a (f)ile or (q)uit: " with no advancing
-	accept array-area
+		display "Enter roman numerals, read a (f)ile or (q)uit: " with no advancing
+		accept array-area
 
-	perform until letter(i) = ' '
-
-		move 0 to val
-
+*		Close the program if they 
 		if letter(1) = 'q' then
-			display ""
-			display "You chose to quit. Goodbye."
-			stop run
-		end-if
+				display ""
+				display "You chose to quit. Goodbye."
+				stop run
+		end-if 
 
+*	If the user chooses to read from a file
 		if letter(1) = 'f' then
-			display ""
-			display "You chose to read from a file."
 
+*		Prompt the user for a filename
+			display ""
+			display "Please enter the filename: "
+			accept filename 
+			display "Filename: " filename
 			open input ifile
 
+*	Continue to read through the file and convert the roman numerals
 			perform
 				until eof-switch = 0
 				read ifile into array-area
@@ -74,11 +83,33 @@ perform until letter(i) = 'q'
 				end-read
 
 				if eof-switch is not = 0
-					display array-area
+					move numeral to array-area
+					perform getvalue
+					display numeral
+					display "Decimal Value: " summation
+					display ""
+
+					move 1 to i
+					move 0 to prev
+					move 0 to summation
 				end-if
 			end-perform
+			move 1 to eof-switch
 			close ifile
-		end-if
+*	If the user enters a roman numeral, just get its value
+		else
+			perform getvalue
+			display "Decimal Value: " summation
+			display ""
+		end-if		
+	end-perform
+		
+	stop run.
+
+getvalue.
+
+*	Keep looping until at the end of the roman numeral
+	perform until letter(i) = ' ' or invalidFlag = 1
 
 		evaluate letter(i)
 			when 'I'
@@ -97,9 +128,10 @@ perform until letter(i) = 'q'
 				move 1000 to val
 			when other
 				display "Invalid Roman Numeral."
+				move 1 to invalidFlag
 		end-evaluate
 
-		 add val to summation
+		add val to summation
 
 		if val > prev then
 			compute summation = summation - 2 * prev
@@ -108,10 +140,5 @@ perform until letter(i) = 'q'
 		add 1 to i
 		move val to prev
 
-	end-perform
-	display "Decimal Value: " summation
-	display ""
-end-perform
-	
-stop run.
+	end-perform.
 
